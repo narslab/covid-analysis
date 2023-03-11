1871e1c62ec947e62a26f177c401291f61a8f5df
Nasko Apostolov <naskoap@gmail.com>
2023-03-10 19:44:34 -0500

    Attempt to generate a readable log likelihood plot by log-scaling y-axis

--- bin/rscripts/rmse_manipulation.r
+++ bin/rscripts/rmse_manipulation.r
@@ -5,10 +5,13 @@ library(tidyr)
 library(tibble)
 library(dplyr)
 library(ggplot2)
+library(scales)
 
 # load forecast with lag params p=7, q=9 based on optimal model
 fcast7.9 <- readRDS("../../models/predictions/model7_9_forecast_n30.RDS")
 rmse <- rmse(fcast7.9)
+
+log_likelihood_df <- read.csv("../../results/3d_scatterplot_input.csv")
 
 rmse.df <- as.data.frame(rmse[30, 1:260])
 rmse.convert.index <- rownames_to_column(rmse.df, var="index")
@@ -39,10 +42,45 @@ df_countries <- rmse.final[rmse.final$country %in% c("US", "ZA", "KR", "DE"), ]
 # convert the data frame from wide to long format, 
 df_long <- gather(rmse.final, variable, value, -country)
 
+# create a labeller function that returns custom labels
+my_labeller <- function(variable, value) {
+  if (variable == "variable") {  # use '==' instead of '='
+    if (value == "cases") {
+      return("Daily COVID counts")
+    } else {
+      return("Change from 100% baseline")
+    }
+  } else {
+    return("test")
+  }
+}
+
 # create a bar plot with country on the x-axis and value on the y-axis
 ggplot(df_long, aes(x = country, y = value)) +
   geom_bar(stat = "identity", position = "dodge") +
   facet_wrap(~ variable, nrow = 5, scales = "free_y") +
-  labs(x = "", y = "") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+
+# treat p and q lags as discrete variables
+log_likelihood_df$p_lag <- factor(log_likelihood_df$p_lag)
+log_likelihood_df$q_lag <- factor(log_likelihood_df$q_lag)
+
+log_likelihood_df_filtered <- log_likelihood_df %>%
+  filter(log_likelihood < 1e+19)
+
+ggplot(log_likelihood_df,aes(x = p_lag, y = log_likelihood + abs(min(log_likelihood)) + 1,
+                            color = q_lag)) + 
+  theme_bw() +
+  geom_point(size = 2, alpha =  0.5) +
+  geom_line(aes(group = q_lag),size = 1.5) + 
+  scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  labs( x = "p lag", 
+        y = "Log likelihood", color = "q lag") +
+  theme(axis.title = element_text(size = 30),
+        axis.text = element_text(size = 25),
+        legend.title = element_text(size = 30),
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size = 25))
 
