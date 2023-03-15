3dfd7917e3224907f64b90ed37fb1762d7039d0a
Nasko Apostolov <naskoap@gmail.com>
2023-03-15 16:54:33 -0400

    Plotted model predictions (train + test) as a facet plot

--- bin/rscripts/plot_model_predictions.r
+++ bin/rscripts/plot_model_predictions.r
@@ -6,33 +6,41 @@ library(ggplot2)
 model_opt <- readRDS("../../models/model7_9.RDS")
 countries <- c("DE", "KR", "US", "ZA")
 
-plot_model_pred <- function(model, country, cases=FALSE) {
+plot_model_pred <- function(model, country) {
   c <- model$args$Data[[country]]
   df <- as.data.frame(c)
   df$day <- 1:nrow(df)
-  if (cases == TRUE) { 
-    df_filtered <- df %>% select(cases, day)
-    y_val <- "Daily COVID-19 counts"
-    vals <- c("cases" = "red")    
-  } else {
-    df_filtered <- df %>% select(-cases)
-    y_val <- "Percent change from baseline"
-    vals <- c("residential" = "blue", 
-              "workplaces" = "green", "transit" = "purple", 
-              "grocery" = "orange")
+  vals <- c("cases" = "red",
+            "residential" = "blue", 
+            "workplaces" = "green", 
+            "transit" = "purple", 
+            "grocery" = "orange")
+  data_long <- gather(df, variable, value, -day)
+  file_name <- paste("../../figures/model_predictions_",country,".png")
+  axis_labels <- function(x) {
+    if (x == "cases") {
+      return("Change in daily COVID-19 cases")
+    } else {
+      return("Percent change from baseline")
+    }
   }
-  data_long <- gather(df_filtered, variable, value, -day)
-  file_name <- paste("../../figures/model_predictions_",country,"_",y_val,".png")
   my_plot <- ggplot(data_long, aes(x = day, y = value, color = variable)) +
     geom_line() +
     ggtitle(country) +
-    labs(x = "Day", y = y_val) +
+    labs(x = "Day", y = "") +
     scale_color_manual(values = vals) +
-    theme(plot.title = element_text(hjust = 0.5))
-  ggsave(file_name, my_plot, device = "png")
+    theme(plot.title = element_text(hjust = 0.5)) +
+    facet_wrap(~ variable, 
+               nrow = 5,
+               scales = "free_y") 
+  ggsave(file_name, my_plot, width = 8, height = 10, device = "png")
 }
+
+
 
 for(c in countries) {
   plot_model_pred(model_opt, c)  
-  plot_model_pred(model_opt, c, TRUE)  
 }
+
+
+
