6431611c25411461a82fa159b6b24975f0fcfbe0
Nasko Apostolov <naskoap@gmail.com>
2023-03-21 13:34:03 -0400

    Fixed log likelihood p/q lag plot

--- bin/rscripts/rmse_manipulation.r
+++ bin/rscripts/rmse_manipulation.r
@@ -5,13 +5,11 @@ library(tidyr)
 library(tibble)
 library(dplyr)
 library(ggplot2)
-library(scales)
+
 
 # load forecast with lag params p=7, q=9 based on optimal model
 fcast7.9 <- readRDS("../../models/predictions/model7_9_forecast_n30.RDS")
 rmse <- rmse(fcast7.9)
-
-log_likelihood_df <- read.csv("../../results/3d_scatterplot_input.csv")
 
 rmse.df <- as.data.frame(rmse[30, 1:260])
 rmse.convert.index <- rownames_to_column(rmse.df, var="index")
@@ -56,31 +54,12 @@ my_labeller <- function(variable, value) {
 }
 
 # create a bar plot with country on the x-axis and value on the y-axis
-ggplot(df_long, aes(x = country, y = value)) +
+rmse_facet <- ggplot(df_long, aes(x = country, y = value)) +
   geom_bar(stat = "identity", position = "dodge") +
   facet_wrap(~ variable, nrow = 5, scales = "free_y") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
-
-# treat p and q lags as discrete variables
-log_likelihood_df$p_lag <- factor(log_likelihood_df$p_lag)
-log_likelihood_df$q_lag <- factor(log_likelihood_df$q_lag)
-
-log_likelihood_df_filtered <- log_likelihood_df %>%
-  filter(log_likelihood < 1e+19)
-
-ggplot(log_likelihood_df,aes(x = p_lag, y = log_likelihood + abs(min(log_likelihood)) + 1,
-                            color = q_lag)) + 
-  theme_bw() +
-  geom_point(size = 2, alpha =  0.5) +
-  geom_line(aes(group = q_lag),size = 1.5) + 
-  scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
-                labels = trans_format("log10", math_format(10^.x))) +
-  labs( x = "p lag", 
-        y = "Log likelihood", color = "q lag") +
-  theme(axis.title = element_text(size = 30),
-        axis.text = element_text(size = 25),
-        legend.title = element_text(size = 30),
-        legend.key.size = unit(2, 'cm'),
-        legend.text = element_text(size = 25))
-
+ggsave(filename = "../../figures/rmse_facet.png",
+       plot = rmse_facet,
+       width = 10, height = 10,
+       device = "png")
