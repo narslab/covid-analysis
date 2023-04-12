8e61560d2bda66ca9487ca342ae8140174d6d6b4
Nasko Apostolov <naskoap@gmail.com>
2023-04-12 02:23:19 -0400

    Minor improvement, more print statements

--- bin/rscripts/test_alternate_specs.r
+++ bin/rscripts/test_alternate_specs.r
@@ -8,6 +8,8 @@ endoList <- readRDS("../../data/tidy/endoList.RDS")
 exoList <- readRDS("../../data/tidy/exoList.RDS")
 var.list <- readRDS("../../data/tidy/var_list.RDS")
 
+params <- list(list(p=6, q=4), list(p=7, q=1), list(p=5, q=7), list(p=4, q=8))
+
 # Set up a parallel backend with the desired number of cores
 cores=detectCores()
 cl <- makeCluster(cores[1]-1) #not to overload your computer
@@ -30,8 +32,6 @@ estModel <- function(p, q, endogenous, exogenous, weights, variables) {
 }
 
 # Run the loop in parallel using foreach
-params <- list(list(p=6, q=4), list(p=7, q=1), list(p=5, q=7), list(p=4, q=8))
-
 results <- foreach(i = 1:length(params), .combine = "list", .packages = "BGVAR") %dopar% {
   p_lag <- params[[i]]$p
   q_lag <- params[[i]]$q
@@ -42,10 +42,12 @@ results <- foreach(i = 1:length(params), .combine = "list", .packages = "BGVAR")
 
 # Save the models
 for (i in seq_along(results)) {
-  p <- params[[i]]$p
-  q <- params[[i]]$q
+  p_lag <- params[[i]]$p
+  q_lag <- params[[i]]$q
+  filename <- paste0("../../models/testing/model", p_lag, "_", q_lag, ".RDS")
   
-  saveRDS(results[[i]], file = paste0("../../models/model", p, "_", q, ".RDS"))
+  print(paste("Saving model with parameters p=", p_lag, " and q=", q_lag, " to ", filename, sep=""))
+  saveRDS(results[[i]], file = filename)
 }
 
 # Stop the parallel backend
