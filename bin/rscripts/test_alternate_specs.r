bd4ea79caa26971ee05f199e38554cb019c6194d
Nasko Apostolov <naskoap@gmail.com>
2023-04-12 02:05:19 -0400

    Run estimation of alternate models in parallel

--- bin/rscripts/test_alternate_specs.r
+++ bin/rscripts/test_alternate_specs.r
@@ -1,3 +1,5 @@
+library(foreach)
+library(doParallel)
 library(BGVAR)
 
 # Load the data
@@ -6,35 +8,44 @@ endoList <- readRDS("../../data/tidy/endoList.RDS")
 exoList <- readRDS("../../data/tidy/exoList.RDS")
 var.list <- readRDS("../../data/tidy/var_list.RDS")
 
-# Set up the parameters for the four models
-params <- list(
-  list(p = 6, q = 4),
-  list(p = 7, q = 1),
-  list(p = 5, q = 7),
-  list(p = 4, q = 8)
-)
+# Set up a parallel backend with the desired number of cores
+cores=detectCores()
+cl <- makeCluster(cores[1]-1) #not to overload your computer
+registerDoParallel(cl)
 
-## Loop over the parameters and estimate and save each model
-for (i in seq_along(params)) {
-  # Extract the lag parameters for this iteration
+
+# Define the function to estimate the model
+estModel <- function(p, q, endogenous, exogenous, weights, variables) {
+  model <- bgvar(Data = endogenous, 
+                 Ex = exogenous,
+                 W = weights,
+                 plag = c(p, q),
+                 draws=100, burnin=100, prior="SSVS", SV=TRUE, 
+                 hold.out = 30,
+                 eigen = 1,
+                 expert = list(cores=4,
+                               variable.list = variables) #specifies which variable is weakly exogenous
+  )
+  return(model)
+}
+
+# Run the loop in parallel using foreach
+params <- list(list(p=6, q=4), list(p=7, q=1), list(p=5, q=7), list(p=4, q=8))
+
+results <- foreach(i = 1:length(params), .combine = "list", .packages = "BGVAR") %dopar% {
+  p_lag <- params[[i]]$p
+  q_lag <- params[[i]]$q
+  
+  estModel(p_lag, q_lag, endoList, exoList, bwList, var.list)
+}
+
+# Save the models
+for (i in seq_along(results)) {
   p <- params[[i]]$p
   q <- params[[i]]$q
   
-  # Estimate the model with the specified lag parameters
-  model <- bgvar(Data = endoList, #endogenous variables
-                   Ex = exoList, # exogenous variables
-                   W = bwList,
-                   plag = c(p, q),
-                   draws=100, burnin=100, prior="SSVS", SV=TRUE, 
-                   hold.out = 30,
-                   eigen = 1,
-                   expert = list(cores=4,
-                                 variable.list = var.list) #specifies which variable is weakly exogenous
-  )
-  
-  # Save the model as an RDS file
-  saveRDS(model, file = paste0("../../models/testing/model", p, "_", q, ".RDS"))
+  saveRDS(results[[i]], file = paste0("../../models/model", p, "_", q, ".RDS"))
 }
 
-
-
+# Stop the parallel backend
+stopCluster(cl)
